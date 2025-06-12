797a16e5c6be0b54f47d1663e64c025eaa264552
Dmikko <johanson.mike@hotmail.com>
2025-06-12 15:26:52 +0200

    Color fix on dark mode - Changed from dynamic boolean from true to false and added static colors.

--- app/src/main/java/com/example/klimaklog/ui/theme/Color.kt
+++ app/src/main/java/com/example/klimaklog/ui/theme/Color.kt
@@ -2,10 +2,7 @@ package com.example.klimaklog.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val DarkTextOnGreen = Color(0xFF003300)
+val KlimaGreen = Color(0xFFC8FAD8)          // Den grønne vi bruger til kort
+val KlimaDarkText = Color(0xFF003300)       // Mørk grøn/brun til læsbarhed på den grønne
