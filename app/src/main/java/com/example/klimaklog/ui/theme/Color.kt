c103e627e64d41a588b75d50204cc8bf842dd088
Dmikko <johanson.mike@hotmail.com>
2025-06-12 16:00:14 +0200

    Color fix on dark mode 2.0 - Lighter color added to dark mode fonts for readability

--- app/src/main/java/com/example/klimaklog/ui/theme/Color.kt
+++ app/src/main/java/com/example/klimaklog/ui/theme/Color.kt
@@ -3,6 +3,6 @@ package com.example.klimaklog.ui.theme
 import androidx.compose.ui.graphics.Color
 
 
-val DarkTextOnGreen = Color(0xFF003300)
+val DarkTextOnGreen = Color(0xFFC8FAD8)
 val KlimaGreen = Color(0xFFC8FAD8)          // Den grønne vi bruger til kort
 val KlimaDarkText = Color(0xFF003300)       // Mørk grøn/brun til læsbarhed på den grønne
